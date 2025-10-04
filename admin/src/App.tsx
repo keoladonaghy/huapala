37dd4b32680c2862974215d8c9afa54cba4e45bf
keoladonaghy <donaghy@hawaii.edu>
2025-10-03 16:24:09 -1000

    Fix React Router basename for GitHub Pages deployment

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/huapala/admin">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
